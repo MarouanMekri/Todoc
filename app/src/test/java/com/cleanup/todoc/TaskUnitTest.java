56ec1efb5212f9fc7c8e53aebd7d70be6dc1c38e
NucleoNNN <mekrimarouan@gmail.com>
2021-11-11 12:25:05 +0100

    Mockito & UnitTests commit

--- app/src/test/java/com/cleanup/todoc/TaskUnitTest.java
+++ app/src/test/java/com/cleanup/todoc/TaskUnitTest.java
@@ -10,19 +10,41 @@ import java.util.Collections;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 public class TaskUnitTest {
+
+    /**
+     * - Initializes 4 Task objects
+     * - Check values of each item
+     */
     @Test
-    public void test_projects() {
+    public void test_task_objects() {
+        // Create tasks objects
         final Task task1 = new Task(1, "task 1", new Date().getTime());
         final Task task2 = new Task(2, "task 2", new Date().getTime());
         final Task task3 = new Task(3, "task 3", new Date().getTime());
         final Task task4 = new Task(4, "task 4", new Date().getTime());
 
+        // Compare IDs
+        assertEquals(1, task1.getProjectId());
+        assertEquals(2, task2.getProjectId());
+        assertEquals(3, task3.getProjectId());
+        assertEquals(4, task4.getProjectId());
+
+        // Compare Names
+        assertEquals("task 1", task1.getName());
+        assertEquals("task 2", task1.getName());
+        assertEquals("task 3", task1.getName());
+        assertEquals("task 4", task1.getName());
     }
 
+    /**
+     * - Initializes 3 Task objects
+     * - Initializes an arrayList containing theses Tasks
+     * - Sort the arraylist using the TaskAZComparator method from TaskComparators class
+     * - Check if sort is correctly done
+     */
     @Test
     public void test_az_comparator() {
         final Task task1 = new Task(1, "aaa", 123);
@@ -40,6 +62,12 @@ public class TaskUnitTest {
         assertSame(tasks.get(2), task2);
     }
 
+    /**
+     * - Initializes 3 Task objects
+     * - Initializes an arrayList containing theses Tasks
+     * - Sort the arraylist using the TaskZAComparator method from TaskComparators class
+     * - Check if sort is correctly done
+     */
     @Test
     public void test_za_comparator() {
         final Task task1 = new Task(1, "aaa", 123);
@@ -57,6 +85,12 @@ public class TaskUnitTest {
         assertSame(tasks.get(2), task1);
     }
 
+    /**
+     * - Initializes 3 Task objects
+     * - Initializes an arrayList containing theses Tasks
+     * - Sort the arraylist using the TaskRecentComparator method from TaskComparators class
+     * - Check if sort is correctly done
+     */
     @Test
     public void test_recent_comparator() {
         final Task task1 = new Task(1, "aaa", 123);
@@ -74,6 +108,12 @@ public class TaskUnitTest {
         assertSame(tasks.get(2), task1);
     }
 
+    /**
+     * - Initializes 3 Task objects
+     * - Initializes an arrayList containing theses Tasks
+     * - Sort the arraylist using the TaskOldComparator method from TaskComparators class
+     * - Check if sort is correctly done
+     */
     @Test
     public void test_old_comparator() {
         final Task task1 = new Task(1, "aaa", 123);
